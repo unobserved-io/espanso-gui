e96b0f9aac2de8ddafe955e0ce209051390e29be
Ricky Kresslein <ricky@unobserved.io>
2023-09-13 10:39:31 +0200

    Add copyright/license comment to each file

--- src/parse_config/mod.rs
+++ src/parse_config/mod.rs
@@ -1,9 +1,23 @@
-/*
- * This file is borrowed from espanso
- *
- * Copyright (C) 2019-2023 Federico Terzi
- *
- */
+// This file is borrowed from espanso
+// Copyright (C) 2019-2023 Federico Terzi
+//
+// and is included as part of
+//
+// espansoGUI - GUI to interface with Espanso
+// Copyright (C) 2023 Ricky Kresslein <ricky@unobserved.io>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 use anyhow::Result;
 use serde::Serialize;
